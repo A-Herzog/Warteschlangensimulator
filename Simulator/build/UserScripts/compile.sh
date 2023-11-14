f4a4169bbde59427934c6f374ace0ba561e0a52a
Alexander Herzog <alexander.herzog@gmx.de>
2023-11-14 08:18:09 +0100

    Update user scripts interfaces compile script

--- Simulator/build/UserScripts/compile.sh
+++ Simulator/build/UserScripts/compile.sh
@@ -6,9 +6,6 @@
 # compile the java files by clicking "Compile" in the
 # plugin folder configuation dialog.
 
-# The release option was introduced in Java 9.
-# If you use a Java 8 JDK, remove the '--release="8"' parameter.
-
 if [ -z "$JAVA_HOME" ]
 then
 	JAVAC_RUN="javac"
@@ -16,7 +13,9 @@ else
 	JAVAC_RUN="${JAVA_HOME}/bin/javac"
 fi
 
-${JAVAC_RUN} --release="8" -cp . ./scripting/java/ClientsInterface.java
-${JAVAC_RUN} --release="8" -cp . ./scripting/java/RuntimeInterface.java
-${JAVAC_RUN} --release="8" -cp . ./scripting/java/SystemInterface.java
-${JAVAC_RUN} --release="8" -cp . ./ExampleClass.java
+${JAVAC_RUN} --release="11" -cp . ./scripting/java/ClientInterface.java
+${JAVAC_RUN} --release="11" -cp . ./scripting/java/ClientsInterface.java
+${JAVAC_RUN} --release="11" -cp . ./scripting/java/OutputInterface.java
+${JAVAC_RUN} --release="11" -cp . ./scripting/java/RuntimeInterface.java
+${JAVAC_RUN} --release="11" -cp . ./scripting/java/SystemInterface.java
+${JAVAC_RUN} --release="11" -cp . ./ExampleClass.java
