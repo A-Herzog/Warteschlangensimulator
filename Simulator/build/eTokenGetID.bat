a11453f4729754613314d60a6bf2715e70f4328c
Alexander Herzog <alexander.herzog@gmx.de>
2025-12-08 09:40:56 +0100

    Updated signing config

--- Simulator/build/eTokenGetID.bat
+++ Simulator/build/eTokenGetID.bat
@@ -1,3 +1,3 @@
-"C:\Program Files\Eclipse Adoptium\jdk-18.0.2.9-hotspot\bin\keytool" -list -keystore NONE -storetype PKCS11 -providerclass sun.security.pkcs11.SunPKCS11 -providerArg ./eToken.cfg
+keytool -v -list -keystore NONE -storetype PKCS11 -providerclass sun.security.pkcs11.SunPKCS11 -providerArg ./eToken.cfg
 
 pause
